01d613b855e009d0b63eaf08597337a3b9a64143
Andrea Martz <14165788+andreamartz@users.noreply.github.com>
2023-11-07 15:01:34 -0600

    Get back to starting project

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,11 @@
 import Head from 'next/head';
 
 import '../styles/globals.css';
-import Layout from '../components/layout/layout';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Head>
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-      </Head>
-      <Component {...pageProps} />
-    </Layout>
+    // <Component {...pageProps} />
+    <h1>App</h1>
   );
 }
 
